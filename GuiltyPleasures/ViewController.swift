f00c49b23633c8d659e460064181f9d3ec055340
Lorman Lau <lormanlau@gmail.com>
2017-10-22 01:14:19 -0700

    Added the api call for sunchrony

--- GuiltyPleasures/ViewController.swift
+++ GuiltyPleasures/ViewController.swift
@@ -9,10 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBAction func apicall(_ sender: UIButton) {
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://syf2020.syfwebservices.com/v1_0/next_purchase") as! URL)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let bodyString = "{\"accountNumber\": 1337 }"
+        let bodyData = bodyString.data(using: .utf8)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData
+        
+        print(NSString(data: request.httpBody!, encoding: String.Encoding.utf8.rawValue)!)
+        print(request.allHTTPHeaderFields!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            print(data!)
+            print(response!)
+            guard error == nil else {
+                print(error)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+            let jsonString = String(describing: json)
+            
+            print(jsonString)
+        }
+        
+        task.resume()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
